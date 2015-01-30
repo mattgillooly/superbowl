6d5b91f4ec5c6b99ee9211b8899a582b5a1c7077
Matt Gillooly <matt@swipely.com>
2015-01-30 17:49:00 -0500

    scraper for historic superbowl score data

---
+++ parse_box_scores.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'nokogiri'
+
+doc = Nokogiri::HTML(ARGF.read)
+
+tables = doc.xpath('//*[@id="content"]/div/div/table/tbody/tr/td/table')
+winning_squares_tables = tables.select{|t| t.to_s.include? 'Winner'}
+
+winning_squares_tables.each do |table|
+  labels, digit_pairs = table.xpath('.//td').map(&:text).each_slice(4).to_a
+
+  labels.zip(digit_pairs).each do |label, digit_pair|
+    unless label.match(/Quarter Winner/)
+      fail "unexpected label: #{label}"
+    end
+
+    puts "Q#{label[0]},#{digit_pair}"
+  end
+end

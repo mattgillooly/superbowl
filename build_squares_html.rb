506955bd0233fcc60624940878b0459635589f1a
Matt Gillooly <matt@swipely.com>
2015-01-30 20:36:37 -0500

    add table with contestant stats

--- build_squares_html.rb
+++ build_squares_html.rb
@@ -24,7 +24,7 @@ opt_parser.parse!
 fail "missing squares file" unless squares_file
 fail "missing box-scores file" unless box_scores_file
 
-squares = CSV.open(squares_file, col_sep: "\t")
+SQUARES = CSV.open(squares_file, col_sep: "\t").to_a
 
 BOX_SCORES = CSV.open(box_scores_file, headers: %w(quarter x y)).to_a
 BOX_SCORE_COUNTS = Array.new(10) { Array.new(10) { 0 } }
@@ -46,6 +46,30 @@ end
 def grid_color(row_index, col_index)
   hex_val = 15 - (ratio(row_index, col_index) * 100).to_i
   hex_val.to_s(16) * 6
+end
+
+class Contestant < Struct.new(:name, :expected_payout, :num_squares)
+  def total_investment
+    10.0 * num_squares
+  end
+
+  def expected_roi
+    100 * (expected_payout - total_investment) / total_investment
+  end
+end
+
+def contestants
+  contestants_by_name = {}
+
+  SQUARES.each_with_index do |row, row_index|
+    row.each_with_index do |owner, col_index|
+      c = contestants_by_name[owner] ||= Contestant.new(owner, 0, 0)
+      c.expected_payout += ratio(row_index, col_index) * 1000
+      c.num_squares += 1
+    end
+  end
+
+  contestants_by_name.values.sort_by(&:expected_payout).reverse
 end
 
 renderer = ERB.new(File.read('squares.html.erb'))

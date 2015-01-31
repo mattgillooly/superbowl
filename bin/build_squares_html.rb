e7b40e30f8453d03b36fef705d1fda3aa983c4cf
Matt Gillooly <matt@swipely.com>
2015-01-30 23:24:47 -0500

    trim down build_squares_html.rb

--- bin/build_squares_html.rb
+++ bin/build_squares_html.rb
@@ -4,37 +4,25 @@ require 'erb'
 require 'optparse'
 require 'superbowl'
 
-squares_file = nil
-box_scores_file = nil
+grid = nil
+box_score_history = nil
 
 opt_parser = OptionParser.new do |opts|
   opts.banner = 'Usage: build_squares_html.rb [options]'
 
   opts.on('-sFILE', '--squares=FILE', 'File containing square owners') do |f|
-    squares_file = f
+    grid = Superbowl::Grid.parse(f)
   end
 
   opts.on('-bFILE', '--box-scores=FILE', 'File with historic box scores') do |f|
-    box_scores_file = f
+    box_score_history = Superbowl::BoxScoreHistory.parse(f)
   end
 end
 
 opt_parser.parse!
 
-fail 'missing squares file' unless squares_file
-fail 'missing box-scores file' unless box_scores_file
-
-b = binding
-
-b.local_variable_set(
-  :grid,
-  Superbowl::Grid.parse(squares_file)
-)
-
-b.local_variable_set(
-  :box_score_history,
-  Superbowl::BoxScoreHistory.parse(box_scores_file)
-)
+fail 'missing squares file' unless grid
+fail 'missing box-scores file' unless box_score_history
 
 renderer = ERB.new(File.read('squares.html.erb'))
-puts renderer.result(b)
+puts renderer.result
